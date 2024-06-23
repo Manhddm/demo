e8799b7998cd01f52d7665ef2d7391faa24fc3e5
Đồng Đức Mạnh <148237861+Manhddm@users.noreply.github.com>
2024-06-23 22:18:34 +0700

    sua lai cho phu hop voi luc thi

--- chuong1/newtonlui.cpp
+++ chuong1/newtonlui.cpp
@@ -1,50 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define flash ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
-#define int long long
-#define TIME (1.0*clock())/CLOCKS_PER_SEC
-vector <double> sp,x,y;
-int n;
-double x_;
-void inputntlui(){
-    cin>>n>>x_;
-    for (int i=0;i<=n;i++){
-        double tmp;
-        cin>>tmp;
-        x.push_back(tmp);
-    }
-    for (int i=0;i<=n;i++){
-        double tmp;
-        cin>>tmp;
-        y.push_back(tmp);
-    }
+void nhap(int n, float x[],float y[]){
+    for (int i=0;i<=n;i++) cin>>x[i];
+    for (int i=0;i<=n;i++) cin>>y[i];
 }
-void SP(){
+void xuat(int n,float x[]){
+    for (int i=0;i<=n;i++) cout<<x[i]<<" ";
+    cout<<endl;
+
+}
+void SP(float sp[],float x[],float y[],int n){
     int c=0;
-    while (sp.size()<=n){
-        sp.push_back(y[n-c]);
-        for (int i=0;i<=n-c;i++){
-            y[i]=y[i+1]-y[i];
-        }
-        c++;
+    while (c<=n)
+    {
+        sp[c]=y[n-c];
+        for (int i=0;i<n-c;i++) y[i]=y[i+1]-y[i];
+        c++; 
     }
+    
 }
-void slove(){
-    SP();
-    double p=sp[0];
-    double t=(x_-x[n])/(x[1]-x[0]),k;k=t;
-    int gt=1;
+float F(float x_,float x[],float sp[],int n){
+    float t=(x_-x[n])/(x[1]-x[0]),k;k=t;
+    float p=sp[0];
     for (int i=1;i<=n;i++){
-        p+=t*sp[i]/gt;
-        gt*=i+1;
-        t*=k+1;
-        k++;
+        p+=t*sp[i];
+        t*=(k+1)/(i+1);
+        k+=1;
     }
-    cout<<p<<endl;
+    return p;
 }
 main(){
-    flash;
-    inputntlui();
-    slove();
-    cerr<<endl<<TIME;
+    int n;cin>>n;
+    float x[n+3],y[n+3],sp[n+3];
+    nhap(n,x,y);
+    SP(sp,x,y,n);
+    cout<<F(1.003,x,sp,n);
 }
